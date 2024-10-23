7d3b8d49d1f5b7e13735d26420a6f68db7df8022
DoyleAaron <aarondoyler101@gmail.com>
2024-10-23 21:26:22 +0100

    Junit setup
    I have finallly setup junit and written my first test, i am using junit 9

--- cm/DoyleAaronTest1.java
+++ cm/DoyleAaronTest1.java
@@ -6,14 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DoyleAaronTest1 {
-    class periodTests {
         @Test
         void PeriodClassTestNumber1() {
-
-            Period period = new Period(5, 10);
-
+            int startHour = 8;
+            Period period = new Period(8, 10);
+            assertNotNull(period);
+            assertEquals(startHour, period.getStartHour());
         }
-    }
 
     class rateTests {
         @Test
