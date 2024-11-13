7f80eace5799406f8ba912f7b7d63c0efd82fbce
DoyleAaron <aarondoyler101@gmail.com>
2024-11-13 13:41:10 +0000

    Setting up base test files

---
+++ cm/DoyleAaronPeriodTests2.java
@@ -0,0 +1,140 @@
+package cm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DoyleAaronPeriodTests2 {
+    @Test
+    void PeriodClassTestNumber1() {
+        // This is to test a valid start hour
+        int startHour = 8;
+        Period period = new Period(8, 10);
+        assertNotNull(period);
+    }
+
+    @Test
+    void PeriodClassTestNumber2() {
+        //This is to test a valid end hour
+        int endHour = 12;
+        Period period = new Period(8, 12);
+        assertNotNull(period);
+    }
+
+    @Test
+    void PeriodClassTestNumber3() {
+        // This tests an invalid start hour
+        int startHour = -1;
+        int endHour = 12;
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Period(startHour, endHour);
+        });
+    }
+
+    @Test
+    void PeriodClassTestNumber4() {
+        // This tests an invalid end hour
+        int startHour = 8;
+        int endHour = 25;
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Period(startHour, endHour);
+        });
+    }
+
+    @Test
+    void PeriodClassTestNumber5() {
+        // This is testing for a valid upper boundary
+        int endHour = 24;
+        Period period = new Period(20, 24);
+        assertNotNull(period);
+    }
+
+    @Test
+    void PeriodClassTestNumber6() {
+        // This is testing for a valid lower boundary
+        int startHour = 0;
+        Period period = new Period(0, 5);
+        assertNotNull(period);
+    }
+
+    @Test
+    void PeriodClassTestNumber7() {
+        // This is testing for when the start time is greater than the end time
+        int startHour = 12;
+        int endHour = 8;
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Period(startHour, endHour);
+        });
+    }
+
+    @Test
+    void PeriodClassTestNumber8() {
+        // This is testing for when the start and end time are the same
+        int startHour = 12;
+        int endHour = 12;
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Period(startHour, endHour);
+        });
+    }
+
+    @Test
+    void OverlapsMethodTestNumber1() {
+        // This to test when there is an overlap occurring between the periods
+        Period period1 = new Period(12,15);
+        Period period2 = new Period(12,17);
+
+        assertTrue(period1.overlaps(period2));
+    }
+
+    @Test
+    void OverlapsMethodTestNumber2() {
+        // This is to test when there is no overlap between the periods
+        Period period1 = new Period(12,15);
+        Period period2 = new Period(20,23);
+
+        assertFalse(period1.overlaps(period2));
+    }
+
+    @Test
+    void OverlapsMethodTestNumber3() {
+        // This is to test no overlap on the boundary of the periods
+        Period period1 = new Period(12,15);
+        Period period2 = new Period(15,18);
+
+        assertFalse(period1.overlaps(period2));
+    }
+
+    @Test
+    void OverlapsMethodTestNumber4() {
+        // This is to test when the periods are the exact same
+        Period period1 = new Period(12,15);
+        Period period2 = new Period(12,15);
+
+        assertTrue(period1.overlaps(period2));
+    }
+
+    @Test
+    void DurationMethodTestNumber1() {
+        // This is to test a valid duration
+        int duration = 8;
+        Period period = new Period(12,20);
+        assertEquals(duration, period.duration());
+    }
+
+    @Test
+    void DurationMethodTestNumber2() {
+        // This is to test duration with the lower boundary
+        int duration = 5;
+        Period period = new Period(0,5);
+        assertEquals(duration, period.duration());
+    }
+
+    @Test
+    void DurationMethodTestNumber3() {
+        // This is to test duration with the upper boundary
+        int duration = 4;
+        Period period = new Period(20,24);
+        assertEquals(duration, period.duration());
+    }
+}
---
+++ cm/DoyleAaronRateTests2.java
@@ -0,0 +1,498 @@
+package cm;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+public class DoyleAaronRateTests2 {
+    @Test
+    void RateClassTestNumber1() {
+        // Testing for a valid car park kind
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(1);
+
+        Rate rate = new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        assertNotNull(rate);
+    }
+
+    @Test
+    void RateClassTestNumber2() {
+        // Testing for a valid hourlyNormalRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(5);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(2);
+
+        Rate rate = new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        assertNotNull(rate);
+    }
+
+    @Test
+    void RateClassTestNumber3() {
+        // Testing for a valid hourlyReducedRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(6);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(4);
+
+        Rate rate = new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        assertNotNull(rate);
+    }
+
+    @Test
+    void RateClassTestNumber4() {
+        // This is testing for lower boundary of hourlyNormalRate & hourlyReducedRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(0);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(0);
+
+        Rate rate = new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        assertNotNull(rate);
+    }
+
+    @Test
+    void RateClassTestNumber5() {
+        // This is testing for upper boundary of hourlyNormalRate & hourlyReducedRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(10);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(10);
+
+        Rate rate = new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        assertNotNull(rate);
+    }
+
+    @Test
+    void RateClassTestNumber6() {
+        // This is testing for when hourlyNormalRate and hourlyReducedRate are the same value
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(8);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(8);
+
+        Rate rate = new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        assertNotNull(rate);
+    }
+
+    @Test
+    void RateClassTestNumber7() {
+        // This is testing for an invalid hourlyNormalRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(50);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(10);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);}
+        );
+    }
+
+    @Test
+    void RateClassTestNumber8() {
+        // This is testing for an invalid hourlyReducedRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(5);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(25);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);}
+        );
+    }
+
+    @Test
+    void RateClassTestNumber9() {
+        // This is testing for the invalid lower boundary on hourlyNormalRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(-1);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(5);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);}
+        );
+    }
+
+    @Test
+    void RateClassTestNumber10() {
+        // This is testing for the invalid upper boundary on hourlyNormalRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(11);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(5);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);}
+        );
+    }
+
+    @Test
+    void RateClassTestNumber11() {
+        // This is testing for the invalid lower boundary on hourlyReducedRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(-1);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);}
+        );
+    }
+
+    @Test
+    void RateClassTestNumber12() {
+        // This is testing for the invalid lower boundary on hourlyReducedRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(11);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);}
+        );
+    }
+
+    @Test
+    void RateClassTestNumber13() {
+        // This is testing for when hourlyReducedRate is greater than hourlyNormalRate
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(5);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {new Rate(kind, normalPeriod, reducedPeriod, hourlyNormalRate, hourlyReducedRate);}
+        );
+    }
+
+    @Test
+    void RateClassTestNumber14() {
+        // This is testing for when hourlyReducedRate & hourlyNormalRate are both null
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(-1);
+
+        assertThrows(NullPointerException.class,
+                () -> {new Rate(kind, normalPeriod, reducedPeriod, null, null);}
+        );
+    }
+
+    @Test
+    void CalculateMethodTestNumber1() {
+        // This is testing the price is calculating correctly for a full normal period stay
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(1);
+
+        Rate rate = new Rate(kind, reducedPeriod, normalPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        Period periodStay = new Period(10,12);
+
+        BigDecimal expectedOutcome = new BigDecimal("4.00"); // Putting this in quotations was suggested by chatGPT as I was having issues with the leading 0's at the end
+
+        assertEquals(expectedOutcome, rate.calculate(periodStay));
+    }
+
+    @Test
+    void CalculateMethodTestNumber2() {
+        // This is testing the price is calculating correctly for a full reduced period stay
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(1);
+
+        Rate rate = new Rate(kind, reducedPeriod, normalPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        Period periodStay = new Period(21,23);
+
+        BigDecimal expectedOutcome = new BigDecimal("2.00");
+
+        assertEquals(expectedOutcome, rate.calculate(periodStay));
+    }
+
+    @Test
+    void CalculateMethodTestNumber3() {
+        // This is testing the price is calculating correctly for a stay during the free period
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(1);
+
+        Rate rate = new Rate(kind, reducedPeriod, normalPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        Period periodStay = new Period(1,5);
+
+        BigDecimal expectedOutcome = new BigDecimal("0.00");
+
+        assertEquals(expectedOutcome, rate.calculate(periodStay));
+    }
+
+    @Test
+    void CalculateMethodTestNumber4() {
+        // This is testing the price is calculating correctly for a stay during the normal and reduced period
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(1);
+
+        Rate rate = new Rate(kind, reducedPeriod, normalPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        Period periodStay = new Period(19,21);
+
+        BigDecimal expectedOutcome = new BigDecimal("4.00");
+
+        assertEquals(expectedOutcome, rate.calculate(periodStay));
+    }
+
+    @Test
+    void CalculateMethodTestNumber5() {
+        // This is testing the price is calculating correctly for a stay during the normal and free period
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 24;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(1);
+
+        Rate rate = new Rate(kind, reducedPeriod, normalPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        Period periodStay = new Period(8,10);
+
+        BigDecimal expectedOutcome = new BigDecimal("2.00");
+
+        assertEquals(expectedOutcome, rate.calculate(periodStay));
+    }
+
+    @Test
+    void CalculateMethodTestNumber6() {
+        // This is testing the price is calculating correctly for a stay during the reduced and free period
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 22;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(1);
+
+        Rate rate = new Rate(kind, reducedPeriod, normalPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        Period periodStay = new Period(21,24);
+
+        BigDecimal expectedOutcome = new BigDecimal("1.00");
+
+        assertEquals(expectedOutcome, rate.calculate(periodStay));
+    }
+
+    @Test
+    void CalculateMethodTestNumber7() {
+        // This is testing the price is calculating correctly for a stay during the normal, reduced and free period
+        int normalPeriodStart = 9;
+        int normalPeriodEnd = 19;
+        int reducedPeriodStart = 20;
+        int reducedPeriodEnd = 22;
+        CarParkKind kind = CarParkKind.STAFF;
+
+        ArrayList<Period> normalPeriod = new ArrayList<>();
+        normalPeriod.add(new Period(normalPeriodStart, normalPeriodEnd));
+        BigDecimal hourlyNormalRate = new BigDecimal(2);
+
+        ArrayList<Period> reducedPeriod = new ArrayList<>();
+        reducedPeriod.add(new Period(reducedPeriodStart, reducedPeriodEnd));
+        BigDecimal hourlyReducedRate = new BigDecimal(1);
+
+        Rate rate = new Rate(kind, reducedPeriod, normalPeriod, hourlyNormalRate, hourlyReducedRate);
+
+        Period periodStay = new Period(8,21);
+
+        BigDecimal expectedOutcome = new BigDecimal("21.00");
+
+        assertEquals(expectedOutcome, rate.calculate(periodStay));
+    }
+}
